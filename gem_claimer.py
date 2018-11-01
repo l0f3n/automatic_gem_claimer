6de27e4b987a6916c104a9d5f23ba763e7ce9b30
l0f3n <viclo211@student.liu.se>
2018-11-01 13:25:06 +0100

    remove balance

--- gem_claimer.py
+++ gem_claimer.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 from datetime import datetime
 import time
@@ -16,66 +17,61 @@ def claim_gems():
     """ 
     Claims 250 free Gems on 'dragonica-extended.com' every 30 min.
     """ 
-
-    # Set options for webdriver
-    options = webdriver.FirefoxOptions()
-    #options.add_argument('--headless') # Does not open browser
-    #options.add_argument('--mute-audio') # Suppose to mute audio
-
-    # Initialize driver  
-    driver = webdriver.Firefox(executable_path='./geckodriver', 
-                            firefox_options=options)
-
-    # Load starting page
-    driver.get('http://dragonica-extended.com')
-
-    # Login to website
-    driver.find_element_by_name('user_name').send_keys(USERNAME)
-    driver.find_element_by_name('password').send_keys(PASSWORD)
-    driver.find_element_by_name('login').click()
-
-    # Wait for expected condition for 30 sec, otherwise raise exception 
-    wait = WebDriverWait(driver, 30)
-
-    # Navigate to the page with claim button. If these elements cannot be
-    # found then it means that the user is not logged in.
-    try:
-        (wait.until(EC.presence_of_element_located(
-                    (By.ID, 'navi-menu-button-2')))).click()
-        driver.switch_to_frame('ifrm')
-    except:
-        print('Incorrect username or password')
-        driver.quit()
-        sys.exit()
-
-    # Save old balance to notice changes after claiming
-    old_balance = (wait.until(EC.presence_of_element_located(
-        (By.ID, 'CashDP')))).text
-
-    # Claim Gems
-    (wait.until(EC.presence_of_element_located(
-        (By.CLASS_NAME, 'btn_claim')))).click()
     
-    # Wait until the amount of Gems changes. If they dont change then that
-    # probably means that it was less than 30 min before last claim. It will
-    # then try to claim again in 5 min. Otherwise claim again in 30 min.
     try:
-        wait.until(EC.presence_of_element_located((By.ID, 'CashDP')))
-        wait.until_not(EC.text_to_be_present_in_element(
-                (By.ID, 'CashDP'), old_balance))
-        new_balance = driver.find_element_by_id('CashDP').text
-    except:
+
+        # Set options for webdriver
+        options = webdriver.FirefoxOptions()
+        #options.add_argument('--headless') # Does not open browser
+        options.add_argument('--mute-audio') # Suppose to mute audio
+
+        # Initialize driver  
+        driver = webdriver.Firefox(executable_path='./geckodriver', 
+                                firefox_options=options)
+
+        # Load starting page
+        driver.get('http://dragonica-extended.com')
+
+        # Login to website
+        driver.find_element_by_name('user_name').send_keys(USERNAME)
+        driver.find_element_by_name('password').send_keys(PASSWORD)
+        driver.find_element_by_name('login').click()
+
+        # Wait for expected condition for 60 sec, otherwise raise exception 
+        wait = WebDriverWait(driver, 60)
+
+        # Navigate to the page with claim button. If these elements cannot be
+        # found then it means that the user is not logged in.
+        try:
+            (wait.until(EC.presence_of_element_located(
+                        (By.ID, 'navi-menu-button-2')))).click()
+            driver.switch_to_frame('ifrm')
+        except:
+            print('Incorrect username or password')
+            driver.quit()
+            sys.exit()
+
+        # Claim Gems
+        (wait.until(EC.presence_of_element_located(
+            (By.CLASS_NAME, 'btn_claim')))).click()
+       
+    # If browser timesout just try again in 5 minutes
+    except TimeoutException:
         time = str(datetime.now())[:-7]
-        print('[{}] Error: Trying again in 5 min...'.format(\
-                                                                    time))
+        print('[{}] Browser timeout. Trying again in 5 min...'.format(time))
         scheduler.enter(300, 1, claim_gems)
+
+    # If everything schedule next claim in 30 minutes
     else:
         time = str(datetime.now())[:-7]
-        print('[{}] New balance: {}'.format(time, new_balance))
+        print('[{}] Claimed 250 Gems. Claiming again in 30 min...'.format(\
+                                                                    time))
         scheduler.enter(1800, 1, claim_gems)
+
+    # Whatever happens quit driver 
     finally:
         driver.quit()
-
+        
 
 def handle_command_line_arguments():
     parser = argparse.ArgumentParser(description='Automatic Gem claimer.')
